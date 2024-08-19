1f5e9d465d93f914ab7de5b264ac35f008deeb9e
KokoBell <tumelokoko10@gmail.com>
2024-08-19 19:24:15 +0200

    Improved experience of the shop slider navigation

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,16 +1,29 @@
+function addNav(sectionId) {
+  const targetSection = document.getElementById(sectionId)
+
+  const shopNav = document.querySelector('.shopNav')
+
+  targetSection.appendChild(shopNav)
+}
+
+let siteId = window.location.hash.replace('#', '')
+siteId = siteId === '' ? 'icecream-1' : siteId
+
+addNav(siteId)
+
 let shopNav = document.getElementsByClassName('shopNav')[0]
 
 let shopNavChildren = Array.from(shopNav.children)
 
 for (let i = 0; i < shopNavChildren.length; i++) {
-    shopNavChildren[i].addEventListener('click', () => {
-        shopNavChildren.forEach(child => {
-            if (child.classList != shopNavChildren[i].classList) {
-                child.classList.replace('clicked', 'notClicked')
-            }
-        })
-        shopNavChildren[i].classList.replace('notClicked', 'clicked')
+  shopNavChildren[i].addEventListener('click', () => {
+    shopNavChildren.forEach(child => {
+      if (child.classList != shopNavChildren[i].classList) {
+        child.classList.replace('clicked', 'notClicked')
+      }
     })
+    shopNavChildren[i].classList.replace('notClicked', 'clicked')
+  })
 }
 
 const shopNavLinks = document.querySelectorAll('.shopNav a');
@@ -27,4 +40,10 @@ function handleShopNavLinkClick(event) {
   document.querySelector(targetElementId).scrollIntoView({
     behavior: 'smooth',
   });
+
+  const siteId = targetElementId.replace('#', '')
+
+  setTimeout(() => {
+    addNav(siteId)
+  }, 300)
 }
